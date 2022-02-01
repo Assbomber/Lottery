c7e9fbee86bad40b8d97f0409347a00b40e6a837
Aman <amankumarproductions@gmail.com>
2022-02-01 10:06:21 +0530

    added Login

--- Backend/src/routes/auth.ts
+++ Backend/src/routes/auth.ts
@@ -1,44 +1,36 @@
 import express,{Router} from "express";
 import User,{userValidator} from "../schemas/userSchema";
 import jwt from "jsonwebtoken";
+import joi, { Err } from "joi";
 import bcrypt from "bcrypt";
+import log from "../utils/logger";
 const route:Router =express.Router();
 
 
 
 //Register route
-route.post("/register", (req,res):void=>{
-
-    //Validating user info
-    const result=userValidator.validate(req.body);
-
-    //If error while validating...send 400 response
-    if(result.error) res.status(400).send({error:result.error.details});
+route.post("/register", (req,res)=>{
+    log.info("/register :Validating user Schema")
+    const validationResult=userValidator.validate(req.body);
+    if(validationResult.error) return res.status(400).send({error:validationResult.error.details[0].message});
     else{
-            //generating hash for password
-            bcrypt.hash(req.body.password,10,async (err:Error | undefined,hash:string):Promise<void>=>{
-
-                //if error while generating hash...send response 500
-                if(err) res.status(500).send({error:err});
-
-                //else creating new user
+             log.info("/register :Generating hash for password")
+            bcrypt.hash(req.body.password,10,async (err:Error | undefined,hash:string)=>{
+                if(err) return  res.status(500).send({error:err});
                 const user=new User({
                     name:req.body.name,
                     password:hash,
-                    email:req.body.email
+                    email:req.body.email.toLowerCase(),
                 });
 
                 try{
-                    //saving created user to DB
+                    log.info("/register :Saving user to DB")
                     const savedUser=await user.save();
-
-                    //extracting Id,name,email from saved user
                     const {_id,name,email,...others}=savedUser;
-
-                    //sending status 200 and new user details to client
-                    res.status(200).send({_id,name,email});
-                }catch(e){
-                    res.status(500).send({error:e})
+                    return res.status(200).send({_id,name,email});
+                }catch(e:any){
+                    if( e.code===11000) return res.status(400).send({error:"User already exists with same email"});
+                    return res.status(500).send({error:e})
                 }
         });
     }
@@ -46,7 +38,38 @@ route.post("/register", (req,res):void=>{
 
 
 //Login Route
-//implement here----------------
+route.post("/login",async (req,res)=>{
+    const loginSchema=joi.object({
+        email:joi.string().email().required(),
+        password:joi.string().required()
+    })
+    
+    log.info("/login: Validating login schema")
+    const validationResult=loginSchema.validate(req.body)
+    if(validationResult.error) return res.status(400).send({error:validationResult.error.details[0].message});
+    else{
+        log.info("/login: login schema validation successful")
+        try{
+            log.info("/login: fetching user from DB");
+            const dbResult=await User.findOne({email:req.body.email.toLowerCase()});
+            if(!dbResult) return res.status(404).send({error:"No user found"});
 
+            log.info("/login: comparing hash passwords");
+            bcrypt.compare(req.body.password,dbResult ? dbResult.password:"",(err,result)=>{
+                if(!result) return res.status(400).send({error:"Bad credentials"});
+                log.info("/login: Signing JWT");
+                const token:string=jwt.sign({
+                    id:dbResult?.id,
+                    isAdmin:dbResult?.isAdmin
+                },process.env.JWT_SECRET as string,{expiresIn:"1h"});
+                return res.status(200).send({token:token});
+            });
+
+        }catch(e){
+            log.error("/login: An Error occurred while returning token back to client");
+            return res.status(500).send({e});
+        }
+    }
+});
 
 export default route;
--- Backend/src/schemas/userSchema.ts
+++ Backend/src/schemas/userSchema.ts
@@ -6,21 +6,21 @@ interface user{
     name:string;
     password:string;
     email:string;
+    wallet:number;
     isAdmin?:boolean
 }
 
 const userValidator=joi.object<user>({
     name:joi.string().max(120).required(),
     password:joi.string().min(8).max(20).required(),
-    isAdmin:joi.boolean().optional(),
     email:joi.string().email().required()
-    
 })
 
 const userSchema=new mongoose.Schema<user>({
     name:{type:String, required:true}, 
     password:{type:String, required:true},
-    email:{type:String, required:true},
+    email:{type:String, required:true,unique:true},
+    wallet:{type:Number,default:1000},
     isAdmin:{type:Boolean, default:false}
 },{timestamps:true})
 
